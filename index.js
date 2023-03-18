03d23ff395da818b207aa01d9341cadfa49bb925
Denis <denis.were@student.moringaschool.com>
2023-03-18 19:52:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove()
+
+const oldHeader = document.querySelector("h1#main");
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+
+
+oldHeader.parentNode.replaceChild(newHeader, oldHeader);
